068a3a3e7c9a40796f5d0ee7f9815ea5b8d7b456
Rico Ullmann <rico@erinnerungsfragmente.de>
2017-08-12 15:09:08 +0200

    switch to python3 completely

--- README.md
+++ README.md
@@ -9,8 +9,8 @@ This bundle has been tested on the following systems:
 
 | OS          | `[x]` |
 | ----------- | ----- |
-| Fedora 24   | `[x]` |
-| Fedberry 23 | `[ ]` |
+| Fedora 25   | `[x]` |
+| Fedora 26   | `[x]` |
 
 ## Metadata
 
--- items.py
+++ items.py
@@ -1,14 +1,11 @@
 pkg_dnf = {
-    "python": {},
-    "python-devel": {},
-    "python-tools": {},
-    "python2-setuptools": {},
+    "python3": {},
+    "python3-devel": {},
+    "python3-tools": {},
     "python3-setuptools": {},
-    "python2-virtualenv": {},
     "python3-virtualenv": {},
-    "python-pip": {
+    "python3-pip": {
         'needs': [
-            "pkg_dnf:python2-setuptools",
             "pkg_dnf:python3-setuptools",
         ],
     },
@@ -19,6 +16,6 @@ pkg_pip = {}
 for package in node.metadata.get('python', {}).get('pip_packages', {}):
     pkg_pip['{}'.format(package)] = {
         'needs': [
-            "pkg_dnf:python-pip",
+            "pkg_dnf:python3-pip",
         ],
     }
