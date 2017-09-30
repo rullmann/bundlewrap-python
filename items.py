94efd70af1bba2b3ace5d43ca5c829d945172c29
Rico Ullmann <rico@erinnerungsfragmente.de>
2017-09-30 20:06:14 +0200

    proper formatting everywhere

--- items.py
+++ items.py
@@ -1,12 +1,12 @@
 pkg_dnf = {
-    "python3": {},
-    "python3-devel": {},
-    "python3-tools": {},
-    "python3-setuptools": {},
-    "python3-virtualenv": {},
-    "python3-pip": {
+    'python3': {},
+    'python3-devel': {},
+    'python3-tools': {},
+    'python3-setuptools': {},
+    'python3-virtualenv': {},
+    'python3-pip': {
         'needs': [
-            "pkg_dnf:python3-setuptools",
+            'pkg_dnf:python3-setuptools',
         ],
     },
 }
@@ -16,6 +16,6 @@ pkg_pip = {}
 for package in node.metadata.get('python', {}).get('pip_packages', {}):
     pkg_pip['{}'.format(package)] = {
         'needs': [
-            "pkg_dnf:python3-pip",
+            'pkg_dnf:python3-pip',
         ],
     }
